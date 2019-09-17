7036b6085c9a6243d2255090c1d2d4f71a943448
Desmend Jetton <desmendjetton@gmail.com>
2019-09-17 00:12:42 -0500

    Initial commit

---
+++ quotes_login_spider/spiders/login.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+import scrapy
+
+
+class LoginSpider(scrapy.Spider):
+    name = "login"
+    allowed_domains = ["quotes.toscrape.com"]
+    start_urls = (
+        'http://quotes.toscrape.com/login',
+    )
+
+    def parse(self, response):
+        pass
